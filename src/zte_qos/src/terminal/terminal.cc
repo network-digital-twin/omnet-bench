e5ea8b4de84cd2ef49cbf56eacdfeafc969a0db6
WingsUpete <petershen815@126.com>
2024-02-04 01:01:25 +0800

    [PACKET] Uses INET packet rather than message in transmission

--- src/zte_qos/src/terminal/terminal.cc
+++ src/zte_qos/src/terminal/terminal.cc
@@ -1,4 +1,6 @@
 #include "terminal.h"
+#include "inet/common/packet/Packet.h"
+#include "inet/common/packet/chunk/BitCountChunk.h"
 
 using namespace omnetpp;
 
@@ -10,16 +12,18 @@ Define_Module(Terminal);
 void Terminal::initialize() {
     // send a ping message if she is the sender
     if (strcmp("sender", getName()) == 0) {
-        cMessage *msg = new cMessage("ping");
-        send(msg, "out");
+        auto data = inet::makeShared<inet::BitCountChunk>(inet::b(100000000));
+        auto packet = new inet::Packet("ping", data);
+        send(packet, "out");
     }
 }
 
 void Terminal::handleMessage(cMessage *msg) {
     // reply if she is the receiver
     if (strcmp("receiver", getName()) == 0) {
-        cMessage *msg = new cMessage("pong");
-        send(msg, "out");
+        auto data = inet::makeShared<inet::BitCountChunk>(inet::b(100000000));
+        auto packet = new inet::Packet("pong", data);
+        send(packet, "out");
     }
 }
 
