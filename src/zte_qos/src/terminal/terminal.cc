605df82aef1eb56224499f2ef8f49591265d41c6
WingsUpete <petershen815@126.com>
2024-02-04 01:56:27 +0800

    [PACKET] Supports tagging the packet with metadata like tos, id and others

--- src/zte_qos/src/terminal/terminal.cc
+++ src/zte_qos/src/terminal/terminal.cc
@@ -1,4 +1,6 @@
+#include <iostream>
 #include "terminal.h"
+#include "packet/meta_tag_m.h"
 #include "inet/common/packet/Packet.h"
 #include "inet/common/packet/chunk/BitCountChunk.h"
 
@@ -10,10 +12,15 @@ namespace terminal {
 Define_Module(Terminal);
 
 void Terminal::initialize() {
-    // send a ping message if she is the sender
+    // send a ping packet if she is the sender
     if (strcmp("sender", getName()) == 0) {
+        // create a packet of size=100Mb and tos=1 as a packet tag
+        int tos = 114514;
+        std::string title = "ping_tos_" + std::to_string(tos);
         auto data = inet::makeShared<inet::BitCountChunk>(inet::b(100000000));
-        auto packet = new inet::Packet("ping", data);
+        auto packet = new inet::Packet(title.c_str(), data);
+        auto metaTag = packet->addTag<inet::MetaTag>();
+        metaTag->setTos(tos);
         send(packet, "out");
     }
 }
@@ -21,8 +28,12 @@ void Terminal::initialize() {
 void Terminal::handleMessage(cMessage *msg) {
     // reply if she is the receiver
     if (strcmp("receiver", getName()) == 0) {
+        auto recvPkt = check_and_cast<inet::Packet*>(msg);
+        auto recvTag = recvPkt->getTag<inet::MetaTag>();
+        int tos = recvTag->getTos() * 100;
+        std::string title = "pong_tos_" + std::to_string(tos);
         auto data = inet::makeShared<inet::BitCountChunk>(inet::b(100000000));
-        auto packet = new inet::Packet("pong", data);
+        auto packet = new inet::Packet(title.c_str(), data);
         send(packet, "out");
     }
 }
