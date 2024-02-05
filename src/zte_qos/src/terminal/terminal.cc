b7bb06d5cfe95f565563ff9bb8da8bbf323cce73
WingsUpete <petershen815@126.com>
2024-02-06 01:11:49 +0800

    [CHAN] Uses INET's PacketDelayer to properly handle the injection + propagation delay

--- src/zte_qos/src/terminal/terminal.cc
+++ src/zte_qos/src/terminal/terminal.cc
@@ -2,7 +2,7 @@
 #include "terminal.h"
 #include "packet/meta_tag_m.h"
 #include "inet/common/packet/Packet.h"
-#include "inet/common/packet/chunk/BitCountChunk.h"
+#include "inet/common/packet/chunk/ByteCountChunk.h"
 
 using namespace omnetpp;
 
@@ -17,7 +17,7 @@ void Terminal::initialize() {
         // create a packet of size=100Mb and tos=1 as a packet tag
         int tos = 114514;
         std::string title = "ping_tos_" + std::to_string(tos);
-        auto data = inet::makeShared<inet::BitCountChunk>(inet::b(16));
+        auto data = inet::makeShared<inet::ByteCountChunk>(inet::B(2));
         auto packet = new inet::Packet(title.c_str(), data);
         auto metaTag = packet->addTag<inet::MetaTag>();
         metaTag->setTos(tos);
@@ -32,7 +32,7 @@ void Terminal::handleMessage(cMessage *msg) {
         auto recvTag = recvPkt->getTag<inet::MetaTag>();
         int tos = recvTag->getTos() * 100;
         std::string title = "pong_tos_" + std::to_string(tos);
-        auto data = inet::makeShared<inet::BitCountChunk>(inet::b(16));
+        auto data = inet::makeShared<inet::ByteCountChunk>(inet::B(2));
         auto packet = new inet::Packet(title.c_str(), data);
         send(packet, "out");
     }
