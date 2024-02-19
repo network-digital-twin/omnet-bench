6dc2758987810639bc75fed5aca676f6b32cede3
WingsUpete <petershen815@126.com>
2024-02-20 03:29:38 +0800

    [SWITCH:ROUTE] Supports routing table translation from YAML info file's routing spec

--- src/zte_qos/src/switch/routing/RouteSelector.cc
+++ src/zte_qos/src/switch/routing/RouteSelector.cc
@@ -7,26 +7,69 @@ namespace routing {
 
 Define_Module(RouteSelector);
 
+std::map<int, std::vector<std::string>> RouteSelector::loadRoutingInfo() {
+    // TODO: read `infoFile` and get a map: dstId => [portId]
+    // do sth here.
+
+    // TODO: PoC, remove later
+    std::map<int, std::vector<std::string>> testInfo;
+    if (strcmp(getParentModule()->getName(), "s_0") == 0) {
+        testInfo[1] = std::vector<std::string>{"port_0_1__0"};
+        testInfo[2] = std::vector<std::string>{"port_0_2__0"};
+    }
+    return testInfo;
+}
+
+std::map<std::string, int> RouteSelector::getTranslationMap() {
+    std::map<std::string, int> transMap;
+    auto curSwitch = getParentModule();
+    const char *outGateName = "out";    // TODO: read from global const spec files
+    for (int gateId = 0; gateId < curSwitch->gateSize(outGateName); ++gateId) {
+        auto curLink = curSwitch->gate(outGateName, gateId)->getNextGate()->getOwnerModule();
+        auto curPortId = curLink->getProperties()->get("metadata")->getValue("srcPort");
+        transMap[curPortId] = gateId;
+    }
+    return transMap;
+}
+
+void RouteSelector::initRoutingTable() {
+    auto infoMap = loadRoutingInfo();
+    auto transMap = getTranslationMap();
+    for (auto infoIt = infoMap.begin(); infoIt != infoMap.end(); ++infoIt) {
+        std::vector<int> curToGates;
+        for (auto p: infoIt->second) {
+            curToGates.push_back(transMap[p]);
+        }
+        rt[infoIt->first] = curToGates;
+    }
+}
+
+int RouteSelector::selectGate(const std::vector<int> gateIds) {
+    // (current impl: just select the first gate)
+    return gateIds[0];
+}
+
 void RouteSelector::initialize(int stage) {
     PacketClassifierBase::initialize(stage);
     if (stage == 0) {
         sid = par("sid");
-        if (strcmp(getParentModule()->getName(), "s_0") == 0) {
-            rt[1] = 0;
-            rt[2] = 1;
-        }
+        infoFile = par("infoFile").stringValue();
+        initRoutingTable();
     }
 }
 
 int RouteSelector::classifyPacket(inet::Packet *packet) {
     auto tag = packet->getTag<inet::MetaTag>();
     if (tag->getDst() == sid) {
-        // this is the destination, end here -> index N
+        // this is the destination, end here -> index N (NOTE: `outputGates` here is for RouteSelector, not switch)
         return outputGates.size() - 1;
     }
     if (rt.find(tag->getDst()) != rt.end()) {
-        // find the route -> corresponding gate id in the routing table
-        return rt[tag->getDst()];
+        // find the route -> corresponding [gate id] in the routing table
+        auto gateIds = rt[tag->getDst()];
+        // select a candidate gate id as the result
+        auto selectedGateId = selectGate(gateIds);
+        return selectedGateId;
     }
     return -1;
 }
--- src/zte_qos/src/switch/routing/RouteSelector.h
+++ src/zte_qos/src/switch/routing/RouteSelector.h
@@ -3,6 +3,7 @@
 
 #include <omnetpp.h>
 #include <map>
+#include <vector>
 #include "inet/queueing/base/PacketClassifierBase.h"
 
 namespace zte_qos {
@@ -12,10 +13,36 @@ namespace routing {
 class RouteSelector: public inet::queueing::PacketClassifierBase {
 protected:
     int sid;                // switch id
-    std::map<int, int> rt;  // routing table: dst switch id => output gate id (TODO: maybe `map<int, map<string, int>>` for dst + port => gate)
+    std::string infoFile;   // location of the info.yaml file for the switch
+    std::map<int, std::vector<int>> rt;  // routing table: dst switch id => [output gate id]
 protected:
     virtual void initialize(int stage) override;
     virtual int classifyPacket(inet::Packet *packet) override;
+
+    /***
+     * loadRoutingInfo reads the routing specifications from the ".yaml" `infoFile` and
+     * returns a map of dst switch ID => vector of outgoing port IDs of the current switch.
+     */
+    std::map<int, std::vector<std::string>> loadRoutingInfo();
+
+    /***
+     * getTranslationMap iterates through all output gates to find the corresponding outgoing port IDs of the
+     * current switch from the links.
+     */
+    std::map<std::string, int> getTranslationMap();
+
+    /***
+     * initRoutingTable initializes `rt` using `infoFile` and extra information of the outgoing links
+     * from the current switch. First, it loads the routing info map from the specified local YAML file.
+     * Then, it iterates through all output gates to find the corresponding outgoing port IDs specified in
+     * the links. Finally, it translates the routing info map to the desired routing table.
+     */
+    void initRoutingTable();
+
+    /***
+     * selectGate chooses a candidate gate id from the provided gates.
+     */
+    int selectGate(const std::vector<int> gateIds);
 };
 
 } // namespace routing
