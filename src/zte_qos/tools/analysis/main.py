803db226186e5fd600a681ce720eb60375f0b2ef
Peter S <petershen815@126.com>
2024-02-26 03:51:40 +0800

    feat(per-flow-stats): processes packet results into per-flow statistics (#11)
    * [TODO] Specifies TODOs
    
    * [TODO] src-dst pairs should have a list of dict results
    
    * [SINK] Adds src-dst to log results
    
    * [ANA:PKT] Modifies packet result format as src => (dst => list[res_dict])
    
    * [ANA:FLOW] Adds flow processing interface
    
    * [ANA] Wraps packet res analysis (delay, jitter, drop) into a modular function
    
    * [ANA:DROP] Modifies drop to drop_rate
    
    * [ANA] Supports processing delay metrics with different detail level (avg if not detailed)
    
    * [ANA:FLOW] Finishes implementing flow result CSV generation

--- src/zte_qos/tools/analysis/main.py
+++ src/zte_qos/tools/analysis/main.py
@@ -2,16 +2,19 @@ import json
 import os
 import argparse
 import math
-from collections import OrderedDict
 from pathlib import Path
 from typing import Tuple
 from datetime import datetime
+import itertools
 
 import pandas as pd
 
 
+PacketRes = dict[int, dict[int, list[dict]]]
+
+
 class Analyzer:
-    """ Result Analyzer generates corresponding packets.csv and res-simulation.json files. """
+    """ Result Analyzer generates corresponding res-simulation.json, res-packets.csv, and res-flows.csv files. """
     def __init__(self, log_fn: str):
         self.log_fn = log_fn
         if not self.log_fn.endswith('.log') or not os.path.isfile(self.log_fn):
@@ -20,10 +23,10 @@ class Analyzer:
         self.log_fn_prefix = Path(self.log_fn).stem
         self.sim_res, self.pkt_res = self.load_res_from_log()
 
-    def load_res_from_log(self) -> Tuple[dict, OrderedDict[int, dict]]:
+    def load_res_from_log(self) -> Tuple[dict, PacketRes]:
         """ Load simulation and packet results from log files. """
         sim_res = {}
-        pkt_res = OrderedDict()
+        pkt_res: PacketRes = {}
         with open(self.log_fn, 'r') as f:
             while True:
                 line: str = f.readline()
@@ -35,8 +38,46 @@ class Analyzer:
                 if log_json['type'] == 'simulation':
                     sim_res = log_json
                 elif log_json['type'] == 'packet':
-                    pkt_res[log_json['metrics']['pid']] = log_json
+                    src_id = log_json['metrics']['src']
+                    dst_id = log_json['metrics']['dst']
+                    if src_id not in pkt_res:
+                        pkt_res[src_id] = {}
+                    if dst_id not in pkt_res[src_id]:
+                        pkt_res[src_id][dst_id] = []
+                    pkt_res[src_id][dst_id].append(log_json)
         return sim_res, pkt_res
+
+    def get_flat_pkt_res(self) -> list[dict]:
+        """ Get all packet results from as a flatten list. """
+        return list(itertools.chain(*itertools.chain(*[src_dict.values() for src_dict in self.pkt_res.values()])))
+
+    @staticmethod
+    def process_pkt_stats(pkt_res: list[dict], detailed_delay_metrics: bool = True) -> dict:
+        """
+        Process packet statistics given a flat list of packet results.
+        :param pkt_res: a flat list of packet results
+        :param detailed_delay_metrics: whether to calculate all delay metrics (min, max, avg); if not, calculate avg only
+        """
+        pkt_stats = {
+            'num_packets': len(pkt_res)
+        }
+        # calculate packet delay
+        delays = [(res['metrics']['end_ts'] - res['metrics']['start_ts']) for res in pkt_res if res['metrics']['drop'] == 0]
+        avg_delay = sum(delays) / len(delays) if len(delays) > 0 else float('nan')
+        pkt_stats['delay'] = {
+            'min': min(delays) if len(delays) > 0 else float('nan'),
+            'max': max(delays) if len(delays) > 0 else float('nan'),
+            'avg': avg_delay
+        } if detailed_delay_metrics else avg_delay
+        # calculate packet jitter
+        pkt_stats['jitter'] = math.sqrt(sum([
+            math.pow(d - avg_delay, 2) for d in delays
+        ]) / len(delays)) if len(delays) > 0 else float('nan')
+        # get number of packet drops
+        pkt_stats['drop_rate'] = sum([
+            res['metrics']['drop'] for res in pkt_res
+        ]) / len(pkt_res) if len(pkt_res) > 0 else float('nan')
+        return pkt_stats
 
     def gen_sim_res(self) -> None:
         """ Calculate and generate simulation results to .json file. """
@@ -46,19 +87,9 @@ class Analyzer:
         sim_res = {
             'start': str(datetime.fromtimestamp(start_epoch / 1e9)),
             'end': str(datetime.fromtimestamp(end_epoch / 1e9)),
-            'elapsed': (end_epoch - start_epoch) / 1e9
+            'elapsed': (end_epoch - start_epoch) / 1e9,
+            **self.process_pkt_stats(pkt_res=self.get_flat_pkt_res(), detailed_delay_metrics=True)
         }
-        # calculate packet delay
-        delays = [(res['metrics']['end_ts'] - res['metrics']['start_ts']) for res in self.pkt_res.values() if res['metrics']['drop'] == 0]
-        sim_res['delay'] = {
-            'min': min(delays) if len(delays) > 0 else float('nan'),
-            'max': max(delays) if len(delays) > 0 else float('nan'),
-            'avg': sum(delays) / len(delays) if len(delays) > 0 else float('nan')
-        }
-        # calculate packet jitter
-        sim_res['jitter'] = math.sqrt(sum([math.pow(d - sim_res['delay']['avg'], 2) for d in delays]) / len(delays)) if len(self.pkt_res) > 0 else float('nan')
-        # get number of packet drops
-        sim_res['drop'] = sum([res['metrics']['drop'] for res in self.pkt_res.values()])
         print(json.dumps(sim_res, indent=2))
         with open(sim_res_fn, 'w') as f:
             json.dump(sim_res, f, indent=2)
@@ -68,17 +99,35 @@ class Analyzer:
         """ Dump packet-wise results to .csv file. """
         pkt_res_fn = os.path.join(self.log_dir, f'{self.log_fn_prefix}-res-packets.csv')
         pkt_rec: list[dict] = []
-        for res in self.pkt_res.values():
+        for res in self.get_flat_pkt_res():
             pkt_rec.append({
                 **res['metrics'],
                 'module': res['module']
             })
         pkt_df = pd.DataFrame.from_records(
             pkt_rec,
-            columns=['pid', 'start_ts', 'end_ts', 'drop', 'module']
+            columns=['pid', 'src', 'dst', 'start_ts', 'end_ts', 'drop', 'module']
         )
         pkt_df.to_csv(pkt_res_fn, index=False)
         print(f'Successfully generated "{pkt_res_fn}"')
+
+    def gen_flow_res(self) -> None:
+        """ Dump flow-wise results to .csv file. """
+        flow_res_fn = os.path.join(self.log_dir, f'{self.log_fn_prefix}-res-flows.csv')
+        flow_rec: list[dict] = []
+        for src, dst_dict in sorted(self.pkt_res.items()):
+            for dst, res_list in sorted(dst_dict.items()):
+                flow_rec.append({
+                    'src': src,
+                    'dst': dst,
+                    **self.process_pkt_stats(pkt_res=res_list, detailed_delay_metrics=False)
+                })
+        flow_df = pd.DataFrame.from_records(
+            flow_rec,
+            columns=['src', 'dst', 'num_packets', 'delay', 'jitter', 'drop_rate']
+        )
+        flow_df.to_csv(flow_res_fn, index=False, header=False)
+        print(f'Successfully generated "{flow_res_fn}"')
 
 
 if __name__ == '__main__':
@@ -89,3 +138,4 @@ if __name__ == '__main__':
     ana = Analyzer(log_fn=FLAGS.log)
     ana.gen_sim_res()
     ana.gen_pkt_res()
+    ana.gen_flow_res()
